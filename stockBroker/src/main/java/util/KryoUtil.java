4941f1590c200dc68c44f4b77afff85960a896b4
Ursulovic <ivanursulovic@gmail.com>
2023-04-19 21:35:54 +0200

    more progress

--- stockBroker/src/main/java/util/KryoUtil.java
+++ stockBroker/src/main/java/util/KryoUtil.java
@@ -2,11 +2,19 @@ package util;
 
 import com.esotericsoftware.kryo.Kryo;
 import socketServer.messages.RealTimePrice;
+import socketServer.messages.RequestStocks;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class KryoUtil {
 
     public static void registerKryoClasses(Kryo kryo) {
 
+        kryo.register(String.class);
+        kryo.register(ArrayList.class);
+
+        kryo.register(RequestStocks.class);
         kryo.register(RealTimePrice.class);
 
     }
